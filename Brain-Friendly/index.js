feddae0e7419bde5de551b8b05fd3764779d1e09
Satria-WS <extencezone02@gmail.com>
2022-04-05 01:20:39 +0700

    05 april 2022

--- Brain-Friendly/index.js
+++ Brain-Friendly/index.js
@@ -1,5 +1,29 @@
-let scoops = 10;
-while(scoops > 0) {
-    document.write("scoopes <br>");
-    scoops = scoops -1;
+//Obejct oriented
+function getSecret(file, secretPassword) {
+    file.opened = file.opened + 1;
+    if (secretPassword == file.password) {
+        return file.contents;
+    } else {
+        return "Invalid password! No secret for you.";
     }
+};
+
+function setSecret(file, secretPassword, secret) {
+    if (secretPassword == file.password) {
+        file.opened = 0;
+        file.contents = secret;
+    }
+};
+var superSecretFile = {
+    level: "classified",
+    opened: 0,
+    password: 2,
+    contents: "Dr. Evel's next meeting is in Detroit.",
+};
+
+var secret = getSecret(superSecretFile, 2);
+console.log(secret);
+
+setSecret(superSecretFile, 2, "Dr. Evel's next meeting is in Philadelphia.");
+secret = getSecret(superSecretFile, 2);
+console.log(secret);
--- Brain-Friendly/project-game/battleship.js
+++ Brain-Friendly/project-game/battleship.js
@@ -13,30 +13,24 @@ let isSunk = false;
 
 //LOOP: while the ship is not sunk
 while (isSunk == false) {
-  //GET the user's guess
-  guess = prompt("Ready , aim,fire! (enter a number 0-6): ");
-  if (guess >= 0 && guess <= 6) {
-    guesses = guesses + 1;
-    if (guess == location1 || guess == location2 || guess == location3) {
-      alert("HIT!");
-      hits = hits + 1;
-      if (hits == 3) {
-        isSunk = true;
-        alert("You sank my battleship!");
-      }
+    //GET the user's guess
+    guess = prompt("Ready , aim,fire! (enter a number 0-6): ");
+    if (guess >= 0 && guess <= 6) {
+        guesses = guesses + 1;
+        if (guess == location1 || guess == location2 || guess == location3) {
+            alert("HIT!");
+            hits = hits + 1;
+            if (hits == 3) {
+                isSunk = true;
+                alert("You sank my battleship!");
+            }
+        } else {
+            alert("MISS");
+        }
     } else {
-      alert("MISS");
+        alert("Please enter a valid cell number!");
     }
-  } else {
-      alert("Please enter a valid cell number!");
-  }
 }
-
-
-
-
-
-
 
 /* while (isSunk == false) {
     guess = prompt("Ready, aim, fire! (enter a number from 0-6):");
